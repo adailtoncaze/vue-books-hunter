d91f9ef96da2e6f2fa71e052dc2c41a4d15a67a4
adailtoncaze <adailtoncaze@gmail.com>
2022-08-10 02:05:15 -0300

    update design of app

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,10 +2,10 @@
   <footer class="container mx-auto border-t border-gray-600 mt-10 mb-10 flex justify-center">
   
   <div class="mx-auto max-w-screen-xl text-center mt-5">
-      <a href="#" class="flex justify-center items-center text-2xl font-semibold text-white">
-          Books Hunter  
-      </a>
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">Books Hunter</a>. Todos os direitos reservados.</span>
+      <router-link to="/" class="flex justify-center items-center">
+         <img src="@/assets/images/logo-inline.png" alt="Logo da Books Hunter"> 
+      </router-link>
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 Books Hunter. Todos os direitos reservados.</span>
   </div>
 </footer>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,11 @@
     >
       <div class="ml-4 flex space-x-4 items-center">
         <img src="@/assets/images/livro.svg" alt="Livro" class="h-10" />
-        <h1 class="text-yellow-300 text-2xl font-semibold">Meus Livros</h1>
+        <h1 class="text-yellow-500 text-2xl font-semibold">Meus Livros</h1>
       </div>
-      <p class="mr-4 bg-yellow-300 rounded-full px-4 py-3 text-2xl">78</p>
+      <p class="mr-4 bg-yellow-500 rounded-full px-4 py-3 text-2xl font-semibold">78</p>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-5 md:cols-4 lg:cols-4 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-5 md:cols-4 lg:cols-4 gap-6">
       <ItemBook />
       <ItemBook />
       <ItemBook />
