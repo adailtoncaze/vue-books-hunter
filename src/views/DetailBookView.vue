d9333d45658d4cd701b4994d025c7f2ef149f8ce
adailtoncaze <adailtoncaze@gmail.com>
2022-08-14 14:25:59 -0300

    Melhorias da View DetailBook

--- src/views/DetailBookView.vue
+++ src/views/DetailBookView.vue
@@ -2,30 +2,39 @@
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-col">
       <div class="lg:w-4/6 mx-auto">
-        <div class="rounded-lg h-96 overflow-hidden">
+        <div
+          class="rounded-lg flex flex-wrap w-full bg-gradient-to-r from-yellow-500 via-purple-500 to-indigo-500 py-32 px-10 relative mb-4"
+        >
           <img
-            alt="content"
-            class="object-cover object-center h-full w-full"
+            :alt="'Livro ' + data.title"
+            class="rounded-lg w-full object-cover h-full object-center block opacity-20 absolute inset-0"
             :src="data.image_url"
           />
+          <div class="relative z-10 w-full">
+            <h2 class="font-bold text-5xl text-white mb-2">
+              <i class="pi pi-book" style="font-size: 2rem"></i>
+              {{ data.title }}
+            </h2>
+            <p class="text-indigo-800 text-3xl font-medium leading-relaxed">
+              <span v-if="data.collection">
+                <i class="pi pi-th-large" style="font-size: 1.2rem"></i>
+                {{ data.collection }}</span
+              >
+              <span v-if="data.volume" class="ml-2">
+                <i class="pi pi-hashtag" style="font-size: 1.2rem"></i>
+                {{ data.volume }}</span
+              >
+            </p>
+          </div>
         </div>
         <div class="flex flex-col sm:flex-row mt-10">
           <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-            <div
-              class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
-            >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-10 h-10"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
+            <div class="inline-flex w-40 h-40 overflow-hidden">
+              <img
+                class="rounded-full object-cover object-center h-full w-full"
+                src="https://bienaldolivro.com.br/wp-content/uploads/2021/08/Sarah_interna_Cre%CC%81dito-Beowulf-Sheehan.jpeg"
+                alt=""
+              />
             </div>
             <div class="flex flex-col items-center text-center justify-center">
               <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
@@ -33,7 +42,7 @@
               </h2>
               <div class="w-14 h-1 bg-gray-800 rounded mt-1 mb-4"></div>
               <p class="text-base text-yellow-500">
-                {{data.author}}
+                {{ data.author }}
               </p>
             </div>
           </div>
@@ -41,7 +50,7 @@
             class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left"
           >
             <p class="leading-relaxed text-lg mb-4 text-gray-300">
-              {{data.sinopse}}
+              {{ data.sinopse }}
             </p>
             <a class="text-indigo-500 inline-flex items-center"
               >Learn More
@@ -58,6 +67,13 @@
               </svg>
             </a>
           </div>
+        </div>
+        <div class="rounded-lg h-64 overflow-hidden mt-10">
+          <img
+            alt="content"
+            class="object-cover object-center h-full w-full"
+            src="https://dummyimage.com/1200x500"
+          />
         </div>
       </div>
     </div>
